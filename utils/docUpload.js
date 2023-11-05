d79f41bd6ff16118ce4c08059a774750fb3e539f
Satyam Mattoo <96661612+SatyamMattoo@users.noreply.github.com>
2023-11-05 22:19:14 +0530

    Update docUpload.js

--- utils/docUpload.js
+++ utils/docUpload.js
@@ -3,7 +3,7 @@ import multer from 'multer';
 export const upload = multer({
     storage: multer.memoryStorage(), // Store the file in memory for now
     limits:{
-        fileSize:5*1024*1024
+        fileSize:1*1024*1024
     }
 });
   
